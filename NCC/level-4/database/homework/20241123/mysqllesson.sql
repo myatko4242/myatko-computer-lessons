2ba81569fcc0211bae5293f6252c6eefe4c1e198
myatko4242 <myatko4242@gmail.com>
2024-12-11 04:01:40 +0900

    DDOOCP added

--- NCC/level-4/database/homework/20241123/mysqllesson.sql
+++ NCC/level-4/database/homework/20241123/mysqllesson.sql
@@ -182,3 +182,94 @@ where age=4;       -- delete all cats that are age 4.
 delete from Cats
 where age=cat_id;  -- delete all cats that have same age as it's id.
 delete from Cats;  -- delete all cats.
+
+-- CRUD practise lesson
+
+create database shirt_db;
+show databases;
+use shirt_db;
+
+create table shirts
+(shirt_id integer auto_increment primary key,
+article varchar(20) not null,
+colour varchar(20)not null,
+shirt_size varchar(20) not null,
+last_worn integer not null);
+show tables;
+
+insert into shirts(article,colour,shirt_size,last_worn)
+values ('t-shirt', 'white', 'S', 10),
+('t-shirt', 'green', 'S', 200),
+('polo shirt', 'black', 'M', 10),
+('tank top', 'blue', 'S', 50),
+('t-shirt', 'pink', 'S', 0),
+('polo shirt', 'red', 'M', 5),
+('tank top', 'white', 'S', 200),
+('tank top', 'blue', 'M', 15);
+select * from shirts;
+-- add a purple polo shirt size medium last worn 50 days
+insert into shirts (article,colour,shirt_size,last_worn)
+values ('polo shirt','purple','M',50);
+-- show only shirt and colour
+select article,colour from shirts;
+-- show everything of medium size shirt except id
+select article,colour,shirt_size,last_worn from shirts
+where shirt_size = 'M';
+-- update all polo shirts size to Large
+update shirts set shirt_size = 'L'
+where article='polo shirt';
+-- update shirt that was worn 15 days ago to 0
+update shirts set last_worn = 0
+where last_worn= 15;
+-- update all white shirt to size XS and colour off white
+update shirts set shirt_size = 'XS',colour = 'off white'
+where colour = 'white';
+-- delete shirt that was worn 200 days ago
+delete from shirts
+where last_worn = 200;
+-- delete all tank top
+delete from shirts
+where article = 'tank top';
+-- delete all shirts
+delete from shirts;
+-- drop the table
+drop table shirts;
+
+-- #####String function#####
+
+create database book_shop;
+use book_shop;
+
+create table books
+(book_id int auto_increment primary key,
+title varchar(100),
+author_fname varchar(100),
+author_lname varchar(100),
+released_year int,
+stock_quantity int,
+pages int);
+
+insert into books (title,author_fname,author_lname,released_year,stock_quantity,pages)
+values ('The Namesake', 'Jhumpa', 'Lahiri', 2003, 32, 291),
+('Norse Mythology', 'Neil', 'Gaiman',2016, 43, 304),
+('American Gods', 'Neil', 'Gaiman', 2001, 12, 465),
+('Interpreter of Maladies', 'Jhumpa', 'Lahiri', 1996, 97, 198),
+('A Hologram for the King: A Novel', 'Dave', 'Eggers', 2012, 154, 352),
+('The Circle', 'Dave', 'Eggers', 2013, 26, 504),
+('The Amazing Adventures of Kavalier & Clay', 'Michael', 'Chabon', 2000, 68, 634),
+('Just Kids', 'Patti', 'Smith', 2010, 55, 304),
+('A Heartbreaking Work of Staggering Genius', 'Dave', 'Eggers', 2001, 104, 437),
+('Coraline', 'Neil', 'Gaiman', 2003, 100, 208),
+('What We Talk About When We Talk About Love: Stories', 'Raymond', 'Carver', 1981, 23, 176),
+("Where I'm Calling From: Selected Stories", 'Raymond', 'Carver', 1989, 12, 526),
+('White Noise', 'Don', 'DeLillo', 1985, 49, 320),
+('Cannery Row', 'John', 'Steinbeck', 1945, 95, 181),
+('Oblivion: Stories', 'David', 'Foster Wallace', 2004, 172, 329),
+('Consider the Lobster', 'David', 'Foster Wallace', 2005, 92, 343);
+select * from books;
+select concat('pic','kle');        -- this is used to stick letters together and you need to use ''.
+select concat(author_fname,' ',author_lname) as author_name from books;    
+-- this is same but if you use column from table you need to add ' from table'. if you want to give it a name use AS.
+select concat_ws('-',title,author_fname,author_lname) from books;
+-- this one ws use the first '-' as between every value (title-AF-AL). 
+-- also this doesn't have AS so name will be "concat_ws('-',title,author_fname,author_lname)"
